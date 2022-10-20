f67ff5f7a9f8df5ace845646cf6038bcc5a848dc
Sacha <s.hauberdon@gmail.com>
2022-10-20 14:17:54 +0200

    init project react/ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  DefaultOptions,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-function App() {
+import "./App.css";
+import Home from "./pages/Home";
+
+const App = () => {
+  const queryConfig: DefaultOptions = {
+    queries: {
+      useErrorBoundary: true,
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  };
+
+  const queryClient = new QueryClient({ defaultOptions: queryConfig });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <Home />
+      </QueryClientProvider>
     </div>
   );
-}
+};
 
 export default App;
